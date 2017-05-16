a8a54b83f4cf09cc39b4f9f2be57ab84d68bf3ce
defe266 <gabriel@gabrielgomez.es>
2017-05-16 09:41:37 +0200

    FileUploads columns img preview

--- fields/types/fileUpload/FileUploadColumn.js
+++ fields/types/fileUpload/FileUploadColumn.js
@@ -14,8 +14,22 @@ var LocalFileColumn = React.createClass({
 		var href = value && value.url ? value.url : null;
 		var label = value && value.filename ? value.filename : null;
 		return (
-			<ItemsTableCell href={href} padded interior field={this.props.col.type}>
-				<ItemsTableValue>{label}</ItemsTableValue>
+			<ItemsTableCell href={href} padded interior field={this.props.col.type} style={{position :'relative'}}>
+				{/*<ItemsTableValue>{label}</ItemsTableValue>*/}
+				<ItemsTableValue>
+
+					<div style={{
+						"backgroundImage" : "url("+href+"/:/rs=w:32,h:32,m/cr=w:32,h:32"+")",
+						backgroundSize : 'cover',
+						backgroundRepeat : 'no-repeat',
+						backgroundPosition : 'center',
+						width: '32px',
+						height: '32px',
+						/*position: 'absolute',
+    					top: '4px',*/
+					}}/>
+
+				</ItemsTableValue>
 			</ItemsTableCell>
 		);
 	},
